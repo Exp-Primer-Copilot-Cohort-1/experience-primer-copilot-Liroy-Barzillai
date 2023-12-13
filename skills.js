8fb4cf18dd7234fa0c2e4fcdcef46dc4ca8e19fe
Liroy-Barzillai <69144719+Liroy-Barzillai@users.noreply.github.com>
2023-12-13 11:06:50 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,9 @@
+function calculateNumbers(var1, var2)
+{
+    var sum = var1 + var2;
+    var product = var1 * var2;
+    var difference = var1 - var2;
+    var quotient = var1 / var2;
+
+    return [sum, product, difference, quotient];
+}
